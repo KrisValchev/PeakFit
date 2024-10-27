38903b095dd95e34810ef022862e3a80a9eab914
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-10-27 14:09:42 +0200

    Adding error messages for register properties

---
+++ PeakFit.Infrastructure/Constraints/Errors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Infrastructure.Constraints
+{
+    public static class Errors
+    {
+        public const string LengthErrorMessage = "The \"{0}\" field needs to be between {2} and {1} symbols.";
+    }
+}
