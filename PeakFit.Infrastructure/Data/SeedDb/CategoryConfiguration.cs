ff0ab42a88770f2b2305230dde981a575e59d2c9
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-02 17:18:49 +0200

    Category configuration class made

---
+++ PeakFit.Infrastructure/Data/SeedDb/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Infrastructure.Data.SeedDb
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            var data = new SeedData();
+            builder.HasData(new Category[] {data.Core,data.Arms,data.Back,data.Chest,data.Legs,data.Shoulders});
+        }
+    }
+}
