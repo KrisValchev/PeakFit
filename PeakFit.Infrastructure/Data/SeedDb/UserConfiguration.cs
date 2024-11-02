bb23ac6110a3810b5755da2891a2e287dee40097
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-02 17:23:49 +0200

    User configuration class made

---
+++ PeakFit.Infrastructure/Data/SeedDb/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Infrastructure.Data.SeedDb
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData(new ApplicationUser[] { data.User, data.Trainer, data.Admin });
+        }
+    }
+}
