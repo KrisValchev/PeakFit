150ea42c4c615e08441d4e4876cb72f6435dc511
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-02 17:19:08 +0200

    Event configuration class made

---
+++ PeakFit.Infrastructure/Data/SeedDb/EventConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Infrastructure.Data.SeedDb
+{
+    internal class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            var data = new SeedData();
+            builder.HasData(new Event[] { data.Event1 });
+        }
+    }
+}
