d3c2da67be9aee2ace1f38d5468cc267c73bf6c8
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-02 17:19:02 +0200

    Comment configuration class made

---
+++ PeakFit.Infrastructure/Data/SeedDb/CommentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Infrastructure.Data.SeedDb
+{
+    internal class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            var data = new SeedData();
+            builder.HasData(new Comment[] { data.Comment1});
+        }
+    }
+}
