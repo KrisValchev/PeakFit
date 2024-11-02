083881970aad72bf6a916636a4e64558de027070
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-02 17:19:19 +0200

    Exercise configuration class made

---
+++ PeakFit.Infrastructure/Data/SeedDb/ExerciseConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Infrastructure.Data.SeedDb
+{
+    internal class ExerciseConfiguration : IEntityTypeConfiguration<Exercise>
+    {
+        public void Configure(EntityTypeBuilder<Exercise> builder)
+        {
+            var data = new SeedData();
+            builder.HasData(new Exercise[] { data.HackSquat,data.Deadlift,data.BulgarianSplitSquat,data.LegPress});
+        }
+    }
+}
