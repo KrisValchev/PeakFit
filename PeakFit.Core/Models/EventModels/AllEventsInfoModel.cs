05a012160033752f658907c129d3b6d457d08da6
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-04 21:16:52 +0200

    Creating view model for All Events action, showing all events in the database

---
+++ PeakFit.Core/Models/EventModels/AllEventsInfoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PeakFit.Core.Models.EventModels
+{
+    public class AllEventsInfoModel
+    {
+        [Display(Name ="Event identifier")]    
+        public int Id { get; set; }
+
+        [Display(Name = "Event title")]
+        public string Title { get; set; } = null!;
+        [Display(Name = "Event date of starting")]
+        public string StartDate { get; set; } = null!;
+
+        [Display(Name = "Event time of starting")]
+        public string StartHour { get; set; } = null!;
+        [Display(Name = "Event image")]
+        public string ImageUrl { get; set; } = null!;
+        [Display(Name = "Trainer identifier")]
+        public string TrainerId { get; set; } = null!;
+
+    }
+}
---
+++ PeakFit.Core/Services/EventService.cs
@@ -0,0 +1,37 @@
+﻿using PeakFit.Core.Contracts;
+using PeakFit.Core.Models.EventModels;
+using PeakFit.Infrastructure.Common;
+using PeakFit.Infrastructure.Constraints;
+using static PeakFit.Infrastructure.Constraints.EventDataConstraints;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PeakFit.Core.Services
+{
+    public class EventService(IRepository repository) : IEventService
+    {
+       // AllEvents Async method for displaying all events
+        public async Task<IEnumerable<AllEventsInfoModel>> AllEventsAsync()
+        {
+            var events = repository.AllReadOnly<Event>().Where(e => e.IsDeleted == false);
+
+            var allEvents = await events.Select(e => new AllEventsInfoModel()
+            {
+                Id = e.Id,
+                TrainerId = e.UserId,
+                Title = e.Title,
+                StartDate = e.StartDate.ToString(StartDateTimeFormat),
+                StartHour = e.StartHour.ToString(StartHourTimeFormat),
+                ImageUrl = e.ImageUrl
+
+            }).ToListAsync();
+            return allEvents;
+        }
+    }
+}
