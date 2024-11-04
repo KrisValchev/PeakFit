7e0460d42e41b39412ded915fcd4da84c65d531e
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-04 21:16:13 +0200

    Creating interface IEventService

---
+++ PeakFit.Core/Contracts/IEventService.cs
@@ -0,0 +1,14 @@
+﻿using PeakFit.Core.Models.EventModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PeakFit.Core.Models.EventModels.AllEventsInfoModel;
+namespace PeakFit.Core.Contracts
+{
+    public interface IEventService
+    {
+        public Task<IEnumerable<AllEventsInfoModel>> AllEventsAsync();
+    }
+}
