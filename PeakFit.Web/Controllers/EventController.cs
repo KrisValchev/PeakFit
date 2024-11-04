02ac7e75199405d8eaf8e6397bbc62073c558aae
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-04 18:51:22 +0200

    Created event controller

---
+++ PeakFit.Web/Controllers/EventController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeakFit.Infrastructure.Common;
+
+namespace PeakFit.Web.Controllers
+{
+    public class EventController(IRepository repository) : Controller
+    {
+        public Task<IActionResult> All()
+        {
+
+            return View();
+        }
+    }
+}
--- PeakFit.Web/Controllers/HomeController.cs
+++ PeakFit.Web/Controllers/HomeController.cs
@@ -6,19 +6,8 @@ namespace PeakFit.Web.Controllers
 {
 	public class HomeController(ILogger<HomeController> _logger) : Controller
 	{
-		//private readonly ILogger<HomeController> _logger;
-
-		//public HomeController(ILogger<HomeController> logger)
-		//{
-		//	_logger = logger;
-		//}
 
 		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult Privacy()
 		{
 			return View();
 		}
