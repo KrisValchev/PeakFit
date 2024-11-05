73606d1eeb4eb38b28bf5bb1a0ee90ea827d5f9e
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-05 21:07:51 +0200

    ClaimsPrincipalExtension created with methods for finding current user id, and three methods for checking if the role of current user is Administrator,Trainer or User

---
+++ PeakFit.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using static PeakFit.Core.Constants.RoleConstants;
+
+namespace PeakFit.Web.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        // Id method is used to get the user's Id from the ClaimsPrincipal object
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        // IsAdmin method is used to check if the user is an admin
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRole);
+        }
+        // IsAdmin method is used to check if the user is a trainer
+        public static bool IsTrainer(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(TrainerRole);
+        }
+        // IsAdmin method is used to check if the user is an user
+        public static bool IsUser(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(UserRole);
+        }
+    }
+}
