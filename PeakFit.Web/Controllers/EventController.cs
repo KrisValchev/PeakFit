8c4ce5d210659653eb1dfe5d3aedf38db0b94088
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-09 14:24:19 +0200

    Get and Post method for Create added in EventController

--- PeakFit.Web/Controllers/EventController.cs
+++ PeakFit.Web/Controllers/EventController.cs
@@ -10,6 +10,7 @@ using static PeakFit.Web.Extensions.ClaimsPrincipalExtensions;
 using static PeakFit.Core.Constants.RoleConstants;
 using static PeakFit.Infrastructure.Constraints.EventDataConstraints;
 using System.Globalization;
+using System.Diagnostics.Tracing;
 namespace PeakFit.Web.Controllers
 {
 
@@ -82,7 +83,7 @@ namespace PeakFit.Web.Controllers
             DateTime startHour;
             if (DateTime.TryParseExact(model.StartHour.Normalize(), StartHourTimeFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out startHour) == false)
             {
-                ModelState.AddModelError(nameof(model.StartHour), "Invalid date format");
+                ModelState.AddModelError(nameof(model.StartHour), "Invalid time format");
                 return View(model);
             }
             await eventService.EditAsync(id, model);
@@ -90,6 +91,42 @@ namespace PeakFit.Web.Controllers
             //Admin return view implement!
 
             return RedirectToAction(nameof(Details), new { id });
+        }
+
+        [HttpGet]
+        [Authorize(Roles = $"{AdminRole},{TrainerRole}")]
+        public async Task<IActionResult> Create()
+        {
+            var model = new AddEventModel();
+            model.StartDate = DateTime.Now.ToString(StartDateTimeFormat);
+            return View(model);
+        }
+        [HttpPost]
+        [Authorize(Roles = $"{AdminRole},{TrainerRole}")]
+        public async Task<IActionResult> Create(AddEventModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(model); 
+            }
+
+            DateTime startDate;
+            if (DateTime.TryParseExact(model.StartDate.Normalize(), StartDateTimeFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out startDate) == false)
+            {
+                ModelState.AddModelError(nameof(model.StartDate), "Invalid date format");
+                return View(model);
+            }
+
+
+            DateTime startHour;
+            if (DateTime.TryParseExact(model.StartHour.Normalize(), StartHourTimeFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out startHour) == false)
+            {
+                ModelState.AddModelError(nameof(model.StartHour), "Invalid time format");
+                return View(model);
+            }
+            var trainerId=await userManager.GetUserAsync(User);
+            int newEvent=await eventService.CreateAsync(model, trainerId);
+            return RedirectToAction(nameof(Details), new {id=newEvent});   
         }
     }
 }
