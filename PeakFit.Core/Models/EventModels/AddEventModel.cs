29f5396085b4c4a97e20b8e7fcb2365c5fe1c523
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-09 14:23:07 +0200

    AddEventModel created for adding events

---
+++ PeakFit.Core/Models/EventModels/AddEventModel.cs
@@ -0,0 +1,33 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using static PeakFit.Infrastructure.Constraints.EventDataConstraints;
+using static PeakFit.Infrastructure.Constraints.Errors;
+
+
+namespace PeakFit.Core.Models.EventModels
+{
+    public class AddEventModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength, ErrorMessage = LengthErrorMessage)]
+        [Display(Name = "Event title")]
+        public string Title { get; set; } = null!;
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, ErrorMessage = LengthErrorMessage)]
+        [Display(Name = "Event description")]
+        public string Description { get; set; } = null!;
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = "Event image")]
+        public string ImageUrl { get; set; } = null!;
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = "Event start date")]
+        public string StartDate { get; set; } = null!;
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = "Event start hour")]
+        public string StartHour { get; set; } = null!;
+
+
+    }
+}
+
