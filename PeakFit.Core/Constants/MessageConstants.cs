b26ef69d101dc9d7cdcab54b14ff526e19b9b594
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-11 23:42:38 +0200

    MessageConstants added

---
+++ PeakFit.Core/Constants/MessageConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Constants
+{
+    public class MessageConstants
+    {
+        public const string PhoneExists = "Phone number already exists. Enter another one";
+    }
+}
