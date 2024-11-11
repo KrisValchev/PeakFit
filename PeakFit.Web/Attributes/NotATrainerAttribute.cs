694cb03b4125ed2a745c02180676d816ed701fcd
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-11 23:49:16 +0200

    NotATrainerAttribute created

---
+++ PeakFit.Web/Attributes/NotATrainerAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PeakFit.Core.Contracts;
+using PeakFit.Web.Extensions;
+using static PeakFit.Core.Contracts.ITrainerService;
+
+namespace PeakFit.Web.Attributes
+{
+    public class NotATrainerAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            ITrainerService? trainerService = context.HttpContext.RequestServices.GetService<ITrainerService>();
+
+            if (trainerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (trainerService!=null && trainerService.IsInTrainerRoleAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
