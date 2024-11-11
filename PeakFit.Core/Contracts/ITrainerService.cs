48c3e1ce4da48370991a43622ddadcffbf461e53
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-11 23:42:51 +0200

    ITrainerService created

---
+++ PeakFit.Core/Contracts/ITrainerService.cs
@@ -0,0 +1,18 @@
+﻿using PeakFit.Core.Models.TrainerModels;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Contracts
+{
+	public interface ITrainerService
+	{
+		Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber);
+		Task AddPhoneNumberAsync(string id,BecomeTrainerModel model);
+        Task<bool> IsInTrainerRoleAsync(string userId);
+
+    }
+}
