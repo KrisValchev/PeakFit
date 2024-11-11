85e206ac964be0f4e5e8530738274ae0a9e21f26
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-11 23:44:05 +0200

    BecomeTrainerModel created

---
+++ PeakFit.Core/Models/TrainerModels/BecomeTrainerModel.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using static PeakFit.Infrastructure.Constraints.ApplicationUserDataConstraints;
+using static PeakFit.Infrastructure.Constraints.Errors;
+
+namespace PeakFit.Core.Models.TrainerModels
+{
+	public class BecomeTrainerModel
+	{
+
+		[Required(ErrorMessage = RequiredErrorMessage)]
+		[StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength, ErrorMessage = LengthErrorMessage)]
+		[Display(Name = "Phone number")]
+		[Phone]
+		public string PhoneNumber { get; set; } = null!;
+	}
+}
