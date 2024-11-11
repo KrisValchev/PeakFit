289937e1bee0dc480a5ba9b9257feb711fa4a76a
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-11 23:46:30 +0200

    PhoneNumber error added to Error data constraints

--- PeakFit.Infrastructure/Constraints/Errors.cs
+++ PeakFit.Infrastructure/Constraints/Errors.cs
@@ -12,5 +12,6 @@ namespace PeakFit.Infrastructure.Constraints
         public const string LengthErrorMessage = "The \"{0}\" field needs to be between {2} and {1} symbols!";
         // Required Error Message
         public const string RequiredErrorMessage = "The \"{0}\" field is required!";
+
     }
 }
