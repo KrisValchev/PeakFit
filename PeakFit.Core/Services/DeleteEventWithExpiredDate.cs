22c426785fcf0956f63b47099928ae76f5a15c1a
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-14 00:06:07 +0200

    DeleteEventWithExpiredDate created with method for deleting events with expired date

---
+++ PeakFit.Core/Services/DeleteEventWithExpiredDate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeakFit.Core.Contracts;
+using PeakFit.Infrastructure.Common;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Services
+{
+    public class DeleteEventWithExpiredDate(IRepository repository) : IDeleteEventWithExpiredDate
+    {
+        //This method deletes events with expired dates
+        public async Task DeleteExpiredEventsAsync()
+        {
+            var expiredEvents = await repository.All<Event>()
+               .Where(e => e.StartDate < DateTime.Now) // Find events with dates in the past
+               .ToListAsync();
+
+            if (expiredEvents.Any())
+            {
+                foreach (var _event in expiredEvents)
+                {
+                    _event.IsDeleted = true;
+                }
+                await repository.SaveChangesAsync(); // Save changes to delete from the database
+            }
+        }
+    }
+}
