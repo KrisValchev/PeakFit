9c0551ab7af3553ca6836d7c8a60589c86fa8930
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-14 00:07:13 +0200

    ExpiredEventCleanUpService created with BackgroundService checking everyday and deleting methods with expired dates

---
+++ PeakFit.Core/Services/ExpiredEventCleanupService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PeakFit.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Services
+{
+    public class ExpiredEventCleanupService(IServiceProvider serviceProvider) : BackgroundService
+    {
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    var myService = scope.ServiceProvider.GetRequiredService<IDeleteEventWithExpiredDate>();
+                    await myService.DeleteExpiredEventsAsync();
+                }
+
+                // Run every day 
+                await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+            }
+        }
+    }
+}
