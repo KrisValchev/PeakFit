90cff770175488355ed21007a4897af084389178
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-14 20:52:40 +0200

    ITrainingProgram service created

---
+++ PeakFit.Core/Contracts/ITrainingProgramService.cs
@@ -0,0 +1,22 @@
+﻿using PeakFit.Core.Models.EventModels;
+using PeakFit.Core.Models.TrainingProgramModels;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Contracts
+{
+    public interface ITrainingProgramService
+    {
+        Task<IEnumerable<AllTrainingProgramsInfoModel>> AllTrainingProgramsAsync();
+        //Task<EventDetailsModel> DetailsAsync(int id);
+        //Task<bool> ExistAsync(int id);
+        //Task EditAsync(int id, EditEventModel model);
+        //Task<int> CreateAsync(AddEventModel model, ApplicationUser trainer);
+        //Task<EditEventModel> GetEventFromEditEventViewModelByIdAsync(int id);
+        //Task DeleteAsync(int id);
+    }
+}
