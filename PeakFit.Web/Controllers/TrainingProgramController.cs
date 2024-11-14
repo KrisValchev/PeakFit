863c0d6b9302d4d9684e997ed91660ee9d34e0ac
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-14 20:54:46 +0200

    TrainingProgramController created with method All

---
+++ PeakFit.Web/Controllers/TrainingProgramController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PeakFit.Core.Contracts;
+using  PeakFit.Core.Models.TrainingProgramModels;
+namespace PeakFit.Web.Controllers
+{
+    public class TrainingProgramController(ITrainingProgramService programService) : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> All(IEnumerable<AllTrainingProgramsInfoModel> model)
+        {
+            model= await programService.AllTrainingProgramsAsync();
+            return View(model);
+        }
+
+
+    }
+}
