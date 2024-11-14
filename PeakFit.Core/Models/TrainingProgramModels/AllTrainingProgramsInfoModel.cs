187a2ad80651a8c66fa7ee57f3539d6617db8e26
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-14 20:52:56 +0200

    AllTrainingProgramsInfoModel created

---
+++ PeakFit.Core/Models/TrainingProgramModels/AllTrainingProgramsInfoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Models.TrainingProgramModels
+{
+    public class AllTrainingProgramsInfoModel
+    {
+        [Display(Name = "Program identifier")]
+        public int Id { get; set; }
+        [Display(Name = "Program category identifier")]
+        public int CategoryId { get; set; } 
+        [Display(Name = "Program category title")]
+        public string CategoryName { get; set; } = null!;
+        [Display(Name = "Program image")]
+        public string ImageUrl { get; set; } = null!;
+        [Display(Name = "Trainer identifier")]
+        public string TrainerId { get; set; } = null!;
+        [Display(Name = "Trainer username")]
+        public string TrainerUserName { get; set; } = null!;
+        [Display(Name = "Program rating")]
+        public double Rating { get; set; }
+
+    }
+}
