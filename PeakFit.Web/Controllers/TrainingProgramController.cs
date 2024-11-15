095569fb7f075be33e6db03509822b9f9671d286
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-15 14:46:02 +0200

    TrainingProgramController get methods for Details and Add created

--- PeakFit.Web/Controllers/TrainingProgramController.cs
+++ PeakFit.Web/Controllers/TrainingProgramController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using PeakFit.Core.Contracts;
 using  PeakFit.Core.Models.TrainingProgramModels;
+using PeakFit.Core.Services;
+using PeakFit.Web.Attributes;
 namespace PeakFit.Web.Controllers
 {
     public class TrainingProgramController(ITrainingProgramService programService) : Controller
@@ -14,6 +16,28 @@ namespace PeakFit.Web.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (await programService.ExistAsync(id) == false)
+            {
+                //should return BadRequest
+                return RedirectToAction(nameof(All));
+            }
+            var model = await programService.DetailsAsync(id);
+            return View(model);
+        }
+        [HttpGet]
+        [MustBeTrainer]
+        public async Task<IActionResult> Add()
+        {
+            var newProgram = new AddTrainingProgramModel()
+            {
+                Categories = await programService.AllCategoriesAsync()
+            };
 
+            return View(newProgram);
+        }
     }
 }
