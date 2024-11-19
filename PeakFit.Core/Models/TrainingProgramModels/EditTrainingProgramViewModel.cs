920d12b839f7298327577e36f35482a7c718c307
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-19 22:08:24 +0200

    EditTrainingProgramViewModel created

---
+++ PeakFit.Core/Models/TrainingProgramModels/EditTrainingProgramViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PeakFit.Core.Models.CategoryModels;
+using PeakFit.Core.Models.ProgramExerciseModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PeakFit.Infrastructure.Constraints.Errors;
+
+namespace PeakFit.Core.Models.TrainingProgramModels
+{
+    public class EditTrainingProgramViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = "Program category")]
+        public int CategoryId { get; set; }
+        [Display(Name = "Program image")]
+        public string? ImageUrl { get; set; }
+        public IEnumerable<ProgramExerciseAddModel> ProgramExercises { get; set; } = new List<ProgramExerciseAddModel>();
+    }
+}
