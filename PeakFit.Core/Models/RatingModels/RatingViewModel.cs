f2fca66ca05be3172fb2d12ef9b86832092dfb3f
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-22 23:10:07 +0200

    RatingViewModel created

---
+++ PeakFit.Core/Models/RatingModels/RatingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Models.RatingModels
+{
+	public class RatingViewModel
+	{
+		[Required]
+		[Comment("This Rating value")]
+		public int Value { get; set; }
+		[Required]
+		[Comment("The user who rated the program")]
+		public string UserId { get; set; } = null!;
+		[Required]
+		[Comment("The program that is rated")]
+		public int TrainingProgramId { get; set; }
+	}
+}
