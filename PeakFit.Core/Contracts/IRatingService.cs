d24f442286d392d8e7c4566d6a4645409a948d0c
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-22 23:09:38 +0200

    IRatingService interface created with methods for SaveRatingAsync,GetRatingAsync,DeleteRatingAsync,GetProgramRatingStatsAsync

---
+++ PeakFit.Core/Contracts/IRatingService.cs
@@ -0,0 +1,19 @@
+﻿using PeakFit.Core.Models.RatingModels;
+using PeakFit.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Contracts
+{
+	public interface IRatingService
+	{
+		public Task SaveRatingAsync(RatingViewModel rating);
+		public Task<RatingViewModel> GetRatingAsync(string userId,int trainingProgramId);
+		Task DeleteRatingAsync(RatingViewModel rating);
+
+		Task<(double averageRating, int totalRatings)> GetProgramRatingStatsAsync(int trainingProgramId);
+	}
+}
