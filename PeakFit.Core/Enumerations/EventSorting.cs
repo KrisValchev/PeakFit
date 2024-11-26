7476f27fe9a97ad8c1a7f99a51fca7fe71330246
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-26 23:28:24 +0200

    Enumeration for Event Sorting created

---
+++ PeakFit.Core/Enumerations/EventSorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Enumerations
+{
+	public enum EventSorting
+	{
+		DateAscending = 0,
+		DateDescending = 1,
+		ByNameAscending = 2,
+		ByNameDescending = 3,
+	}
+}
