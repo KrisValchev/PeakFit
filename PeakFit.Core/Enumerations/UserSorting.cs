7b5749fd636e62df9b1e646cd88ef62ef8118b4e
Кристиян Вълчев <kvalchev2005@gmail.com>
2024-11-30 20:34:54 +0200

    UserSorting enumeration created

---
+++ PeakFit.Core/Enumerations/UserSorting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakFit.Core.Enumerations
+{
+    public enum UserSorting
+    {
+        EmailAscending = 0,
+        EmailDescending = 1,
+        FirstNameAscending = 2,
+        FirstNameDescending = 3,
+        LastNameAscending = 4,
+        LastNameDescending = 5,
+    }
+}
